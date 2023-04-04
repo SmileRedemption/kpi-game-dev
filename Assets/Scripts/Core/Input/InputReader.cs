a945daf9ca75be718490f4a3ce129996ad88210f
SmileRedemption <ismaylov.den@gmail.com>
2023-04-04 18:02:18 +0300

    add camera and create parallax background

---
+++ Assets/Scripts/Core/Input/InputReader.cs
@@ -0,0 +1,28 @@
+using Core.Enums;
+using UnityEngine;
+
+namespace Core.Input
+{
+    public class InputReader : MonoBehaviour
+    {
+        [SerializeField] private Hero _hero;
+
+        private Vector2 _direction;
+    
+        private void Update()
+        {
+            var horizontal = UnityEngine.Input.GetAxisRaw(InputConstants.Horizontal.ToString());
+            var vertical = UnityEngine.Input.GetAxisRaw(InputConstants.Vertical.ToString());
+
+            if (UnityEngine.Input.GetButtonDown(InputConstants.Jump.ToString()))
+                _hero.Jump();
+
+            _direction = new Vector2(horizontal, vertical);
+        }
+
+        private void FixedUpdate()
+        {
+            _hero.Move(_direction);
+        }
+    }
+}
